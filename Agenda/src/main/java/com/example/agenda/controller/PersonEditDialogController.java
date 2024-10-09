23b0054d8bf94beb57d65313879dffa4baf4e04a
andreesramos <andresrn16@gmail.com>
2024-10-09 12:44:17 +0200

    a

---
+++ Agenda/src/main/java/com/example/agenda/controller/PersonEditDialogController.java
@@ -0,0 +1,47 @@
+package com.example.agenda.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import com.example.agenda.Person;
+import com.example.agenda.DateUtil;
+
+
+public class PersonEditDialogController {
+
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private TextField streetField;
+    @FXML
+    private TextField postalCodeField;
+    @FXML
+    private TextField cityField;
+    @FXML
+    private TextField birthdayField;
+    
+    private Stage dialogStage;
+    private Person person;
+    private boolean okClicked = false;
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+    }
+
+    /**
+     * Sets the stage of this dialog.
+     *
+     * @param dialogStage
+     */
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+}
