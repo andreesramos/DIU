0566c33a7498561b9fc45f080218a9094a88788d
andreesramos <andresrn16@gmail.com>
2024-11-20 20:05:43 +0100

    a

---
+++ Ejemplo/src/main/java/com/example/ejemplo/modelo/repository/TotalInterface.java
@@ -0,0 +1,6 @@
+package com.example.ejemplo.modelo.repository;
+
+public interface TotalInterface {
+
+    float total(Integer unidades,Float precio);
+}
---
+++ Ejemplo/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module com.example.ejemplo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.ejemplo to javafx.fxml;
+    exports com.example.ejemplo;
+    exports com.example.ejemplo.controller;
+    opens com.example.ejemplo.controller to javafx.fxml;
+    exports com.example.ejemplo.modelo.utilidad;
+    opens com.example.ejemplo.modelo.utilidad to javafx.fxml;
+}
